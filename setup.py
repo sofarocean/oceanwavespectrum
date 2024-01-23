1993846fc79fd39e794fdc3f44d09acb1928488b
pieterbartsmit <32703835+pieterbartsmit@users.noreply.github.com>
2024-01-23 13:39:20 -0800

    F/data archive (#5)
    * Bug to add requests/requests-cache to dependencies
    
    * Bug to add requests/requests-cache to dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name="roguewavespectrum",
-    version="0.2.2",
+    version="0.2.3",
     license="Apache 2 License",
     install_requires=[
         "numpy",
@@ -19,6 +19,7 @@ setuptools.setup(
         "pdoc",
         "requests",
         "requests-cache",
+        "matplotlib",
     ],
     extras_require={
         "full": [
