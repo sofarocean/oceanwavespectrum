eebde3ada294a23aefdb5ff06413a1cac58d7fbd
pieterbartsmit <pieterbartsmit@gmail.com>
2024-04-15 15:37:45 -0700

    Up version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name="roguewavespectrum",
-    version="0.2.6",
+    version="0.2.7",
     license="Apache 2 License",
     install_requires=[
         "numpy",
