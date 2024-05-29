bb15c3ad7590918188a513e61758f94ec54a9f50
pieterbartsmit <pieterbartsmit@gmail.com>
2024-05-29 12:56:14 -0700

    - Commit for release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name="roguewavespectrum",
-    version="0.2.9",
+    version="0.2.10",
     license="Apache 2 License",
     install_requires=[
         "numpy",
